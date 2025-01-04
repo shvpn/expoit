172b69ae364881896706f8b7bcb505380e79a3ac
shvpn <oukoksa11@gmail.com>
2025-01-04 22:42:07 +0700

    d

--- part1.py
+++ part1.py
@@ -3,6 +3,7 @@ import struct
 import textwrap
 
 def main():
+    print("Welcome to the Packet Sniffer")
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
     while True:
